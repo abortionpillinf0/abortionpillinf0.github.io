e2552d849b5820ea3e3e620552db35e4babe2e94
abortionpillinf0 <136163674+abortionpillinf0@users.noreply.github.com>
2023-06-14 05:50:35 +0000

    Update json.js

--- resources/condom/json.js
+++ resources/condom/json.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         var uls = document.getElementById('uls');
     for (var i=0;i<data['data'].length;i++) {
         var li = document.createElement('li');
-        li.value = data['data'][i]['tit'];
+        li.innerHTML = data['data'][i]['tit'];
         uls.appendChild(li);
     }
   });
