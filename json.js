9f071a825ff7d244d52c7022cd0d74b293e3ea30
abortionpillinf0 <136163674+abortionpillinf0@users.noreply.github.com>
2023-06-14 03:46:49 +0000

    Update json.js

--- json.js
+++ json.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $.getJSON( "/data.json", function( data ) {
-        var uls = document.findElementById('uls');
+        var uls = document.getElementById('uls');
     for (var i=0;i<data['data'].length;i++) {
         var li = document.createElement('li');
-        li.innerHtml = data['data'][i]['tit'];
+        li.value = data['data'][i]['tit'];
         uls.appendChild(li);
     }
   });
